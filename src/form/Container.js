b3762f8381307fbc92dbb9aaeb6031cda88fd59b
Alexander Kaufmann <ak@vivid-planet.com>
2014-10-21 14:07:56 +0200

    added form container - very useful for adding form fields side by side

---
+++ src/form/Container.js
@@ -0,0 +1,12 @@
+/**
+ * Very useful for adding form fields side by side
+ *
+ * Style margin is derived from ext form fields
+ **/
+Ext.define('Densa.form.Container', {
+    extend: 'Ext.container.Container',
+    layout: 'hbox',
+    style: 'margin-bottom: 5px',
+    alias: 'widget.densa.form.container'
+});
+
