b22c4ed1e8af4e2783e7412b348d624d9b0166cb
Alexander Kaufmann <ak@vivid-planet.com>
2015-08-04 14:40:49 +0200

    daterange bugfix: vtypes will be ignored if allowBlank is true and value is null
    validation for this can be activated with "validateBlank: true" setting

--- src/form/field/vtypes/DateRange.js
+++ src/form/field/vtypes/DateRange.js
@@ -4,21 +4,33 @@ Ext.define('Densa.form.field.vtypes.DateRange', {
         'Ext.form.field.VTypes'
     ]
 });
+/**
+ * vtypes will be ignored if allowBlank is true and value is null
+ * validation for this can be activated with "validateBlank: true" setting
+ */
 Ext.apply(Ext.form.field.VTypes, {
     daterange: function(val, field) {
+        var start, end;
         var date = field.parseDate(val);
 
         if (!date) {
-            return false;
+            if (field.startDateField) {
+                start = field.up('form').down('#' + field.startDateField);
+                start.setMaxValue(null);
+            } else if (field.endDateField) {
+                end = field.up('form').down('#' + field.endDateField);
+                end.setMinValue(null);
+            }
+            return field.allowBlank;
         }
         if (field.startDateField && (!this.dateRangeMax || (date.getTime() != this.dateRangeMax.getTime()))) {
-            var start = field.up('form').down('#' + field.startDateField);
+            start = field.up('form').down('#' + field.startDateField);
             start.setMaxValue(date);
             start.validate();
             this.dateRangeMax = date;
         }
         else if (field.endDateField && (!this.dateRangeMin || (date.getTime() != this.dateRangeMin.getTime()))) {
-            var end = field.up('form').down('#' + field.endDateField);
+            end = field.up('form').down('#' + field.endDateField);
             end.setMinValue(date);
             end.validate();
             this.dateRangeMin = date;
